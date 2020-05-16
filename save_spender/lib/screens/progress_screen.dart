ebe9955c2811f458b10e706720142c3f1362038e
Akmal Hakim Teo <akmal_hakim_teo@hotmail.com>
2020-05-17 02:26:28 +0800

    With Progress chart. TODO: Buttons

--- save_spender/lib/screens/progress_screen.dart
+++ save_spender/lib/screens/progress_screen.dart
@@ -1,20 +1,108 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
+
+import 'package:flutter_thermometer/label.dart';
+import 'package:flutter_thermometer/scale.dart';
+import 'package:flutter_thermometer/setpoint.dart';
+import 'package:flutter_thermometer/thermometer.dart';
+import 'package:flutter_thermometer/thermometer_paint.dart';
+import 'package:flutter_thermometer/thermometer_widget.dart';
+
+
 
 class ProgressScreen extends StatefulWidget {
   @override
   _ProgressScreenState createState() => _ProgressScreenState();
 }
 
+
+
 class _ProgressScreenState extends State<ProgressScreen> {
-
-
 
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-      );
-   
+//    return Scaffold(
+//      body: Center(
+//        child: RaisedButton.icon(      //Tharun
+//            onPressed: () => _openPERXwebpage(context),
+//
+//            icon: Icon(
+//              Icons.card_giftcard
+//            ),
+//            label: Text('Random Prize'),
+//            color: Colors.green
+//        )
+//      )
+//
+//      );
+
+
+    ScaleTick(300);
+
+
+
+    return Scaffold(  //Tharun
+        body: Center(
+            child: SizedBox(
+                width: 300,
+                height: 500,
+
+                child: Thermometer(
+
+                  value: 4800,
+                  //Current savings here
+                  mercuryColor: Colors.green,
+                  minValue: 0,
+                  maxValue: 10000,
+                  //Highest target
+                  setpoint: (
+                      Setpoint(7500, size: 12,
+                          color: Colors.blue,
+                          side: SetpointSide.right)
+                  ),
+                  label: ThermometerLabel("Total Savings (\$)",),
+                  scale: IntervalScaleProvider(2500,thickness: 3.0,fractionDigits: 0,length: 20.0),
+
+                )
+            )
+
+        )
+
+    );
+
+
+
+
+
+
+    _openPERXwebpage(BuildContext context) async {
+//    final url = 'https://light.microsite.perxtech.io/game/3?token=6bead735994ceb98d99b022293423c05f3358dd659a00a08d0243b4ce8c8b77d';
+//    //Fires up PINATA Game
+//    final response = await http.get(url);
+////    dynamic body = jsonDecode(response.body);
+//
+//    // If server returns an OK response, parse the JSON.
+//    if (response.statusCode == 200) {
+//      print(response.body);
+//
+//    }
+//    // If that response was not OK, throw an error.
+//    else {
+//      // throw Exception('Failed to load post');
+//      print('Failed to load post');
+//    }
+//  }
+      const url = 'https://light.microsite.perxtech.io/game/3?token=6bead735994ceb98d99b022293423c05f3358dd659a00a08d0243b4ce8c8b77d';
+      if (await canLaunch(url))
+        launch(url);
+      else {
+        print("Error in launching url");
+      }
+    }
   }
+
 }
